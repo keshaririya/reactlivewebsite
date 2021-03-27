85774bcd7c77d9dca4c396681288741fecd1cb55
Priya_Keshari <priya.k@selectiva.com>
2021-03-28 04:35:16 +0530

    added new line for pull req

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Here First time React Deploy Project on github...</h1>
+      <h1>Added line for pull request...</h1>
     </div>
   );
 }
